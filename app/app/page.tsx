a3186a69ddc44298efff1aa8b4054bee36aac619
AbduTello <133915621+AbduTello@users.noreply.github.com>
2024-12-19 22:08:01 +0000

    add date

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Abdu Tello Get started by editing{" "}
+            Abdu Tello Get started by editing on dec 19th{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
